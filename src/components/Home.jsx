52ebe4d11843f6bea0c609c8eaf81e3770ea54c8
Mansi <mansigg1106@gmail.com>
2023-11-20 22:01:44 +0530

    dark mode

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,14 @@ const Home = () => {
     return (
 
         <div name="home"
-            className="h-screen w-full bg-gradient-to-b from-black via-sky-950 to-sky-900">
+
+            className=" bg-sky-100 h-screen w-full dark:bg-gradient-to-b from-black via-sky-950 to-sky-900">
+            
             <div className="pt-12 max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
                 <div className="flex flex-col justify-center h-full">
-                    <h2 className="text-3xl sm:text-5xl font-bold text-white">Hey, I am Mansi Gupta</h2>
+                    <h2 className="text-3xl sm:text-5xl font-bold dark:text-white">Hey, I am Mansi Gupta</h2>
 
-                    <p className="text-white py-4 max-w-md">
+                    <p className="dark:text-white py-4 max-w-md">
                        Final Year , Computer Science Engineering
                     </p>
 
