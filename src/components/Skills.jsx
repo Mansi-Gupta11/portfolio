2be896e07934cff93a4cd8c51906d5578cd9fe6e
Mansi <mansigg1106@gmail.com>
2024-07-22 23:39:52 +0530

    updated skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -90,7 +90,7 @@ const Skills = () => {
                 skills.map(({id,src,title,style})=>(
 
                 <div key={id} 
-                className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                className={`shadow-md md:hover:scale-105  duration-500 py-2 rounded-lg ${style}`}>
                     <img src={src} alt="" className="w-20 mx-auto" />
                     <p className="mt-4">{title}</p>
                 </div>
